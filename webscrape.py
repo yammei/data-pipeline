12b1a766b09cdbe80505f93353d846f40158a966
yammei <gmeil.antin@gmail.com>
2024-05-13 14:29:06 -0700

    pulls nasdaq ticker list

--- webscrape.py
+++ webscrape.py
@@ -1,17 +1,29 @@
+import os
 import re
 import sys
 import json
 import spacy
 import random
 import requests
+import pandas as pd
+import yfinance as yf
 from bs4 import BeautifulSoup
 from datetime import datetime
+from yahoo_fin import stock_info as si
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 #________________________________________________________________________________________________________________________________________________________________
 
-# Current Issues: Content parsing from get_text() is only returning entries if it contains a words with a length of 15+.
+'''
+    Current Issues:
+    Retreiving stocks from yfinance is returning empty in ./Resources/NASDAQ-Tickers.txt.
+'''
+
+'''
+    Solved Issues:
+    Content parsing from get_text() is only returning entries if it contains a words with a length of 15+.
+'''
 
 #________________________________________________________________________________________________________________________________________________________________
 
@@ -20,6 +32,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 main_source_URL = "https://biztoc.com/light"
 html_name = "web_page.html"
+terminal_vertical_margin_size = 3
+ticker_list = []
 
 #________________________________________________________________________________________________________________________________________________________________
 
@@ -104,6 +118,7 @@ def get_content(source_HTML: str, source_URL: str):
         try:
             elements = soup.find_all(["p", "div", "a"])
             word_threshold = 20
+            max_word_length = 15
             similarity_threshold = 0.4
             blacklist = None
             blacklist_path = "./blacklist.txt"
@@ -116,7 +131,8 @@ def get_content(source_HTML: str, source_URL: str):
                 for element in elements
                 if (
                     len(element.get_text(strip=True).split()) >= word_threshold
-                    # and not any(word in element.get_text(strip=True) for word in blacklist)
+                    and not any(word in element.get_text(strip=True) for word in blacklist)
+                    and not any(len(word) > max_word_length for word in element.get_text(strip=True).split())
                 )
             ]
             unique_texts = [texts[0]]
@@ -142,6 +158,7 @@ def get_content(source_HTML: str, source_URL: str):
 
         except Exception as e:
             print(f"[✗] Error: {e}.")
+
 
     #____________________________________________________________
 
@@ -174,23 +191,79 @@ def curr_time():
 #________________________________________________________________________________________________________________________________________________________________
 
 # Append identified company name from the saved JSON file.
-def get_company(entries: str, file_path: str):
-    print(f"\n[-] Identifying name from: {file_path}.")
+def get_company(file_path: str):
+    print(f"\n[-] Identifying organization name(s) from: {file_path}.")
 
     try:
+        with open(file_path, 'r') as file:
+            file_content = json.load(file)
+
+        entries = '\n'.join(file_content["paragraphs"])
         en_model = spacy.load("en_core_web_sm")
         entries_prime = en_model(entries)
 
-        for entities in entries_prime.ents:
-            print(entities.text, entities.label_)
+        for entity in entries_prime.ents:
+            if entity.label_ == "ORG":
+                print(entity.text)
 
     except Exception as e:
         print(f"[✗] Error: {e}.")
 
 #________________________________________________________________________________________________________________________________________________________________
 
+# Get all stock ticker names from yfinance API.
+def download_tickers(ticker_file_path=f"./Resources/NASDAQ-Tickers.txt"):
+    print(f"\n[-] Downloading all NASDAQ tickers to: {ticker_file_path}.")
+
+    try:
+        if not os.path.exists("./Resources"):
+            os.makedirs("./Resources")
+        elif os.path.exists("./Resources"):
+            ticker_list = si.tickers_nasdaq()
+
+            print(f"[✔] Retrieved {len(ticker_list)} tickers.")
+            with open(ticker_file_path, 'w') as file:
+                for ticker in ticker_list:
+                    file.write(ticker + "\n")
+
+    except Exception as e:
+        print(f"[✗] Error: {e}.")
+
+#________________________________________________________________________________________________________________________________________________________________
+
+# Get all stock ticker names from locally saved ticker list.
+def read_tickers(ticker_file_path=f"./Resources/NASDAQ-Tickers.txt"):
+    print(f"\n[-] Reading all NASDAQ tickers from: {ticker_file_path}.")
+
+    try:
+        if os.path.exists(ticker_file_path):
+            global ticker_list
+            with open(ticker_file_path, 'r') as file:
+                for ticker in file:
+                    ticker_list.append(ticker)
+            print(f"[✔] Retrieved {len(ticker_list)} tickers.")
+
+        elif not os.path.exists(ticker_file_path):
+            print(f"[-] Ticker file not found. Resolving...")
+            download_tickers()
+            read_tickers()
+
+    except Exception as e:
+        print(f"[✗] Error: {e}.")
+
+#________________________________________________________________________________________________________________________________________________________________
+
+# Creates a new terminal line by number_of_spaces given. Just for aesthetics and readibility. :-)
+def push_terminal_space(number_of_spaces):
+    print("\n" * number_of_spaces)
+
+#________________________________________________________________________________________________________________________________________________________________
+
+# [0] Fur Elise
 def main():
     try:
+        read_tickers()
+
         print(f"\nSTAGE 1 [{curr_time()}] ░░░░░░░░░░░░░░░░░░░░░░░░░░░")
 
         # Get list of URLs from source HTML.
@@ -214,13 +287,47 @@ def main():
         print(f"\nSTAGE 3 [{curr_time()}] ░░░░░░░░░░░░░░░░░░░░░░░░░░░")
 
         # Get company name from JSON file.
-        get_company("sample text", file_path)
-
-        # Terminal space push.
-        print("\n\n\n\n\n\n")
+        get_company(file_path)
 
     except Exception as e:
         print(f"Error: {e}")
 
+#________________________________________________________________________________________________________________________________________________________________
+
+# [1] For testing.
+def test_main():
+    print(f"NOTE: You are using the test version of main(). Please swap to main function version 0 for main().")
+
+    try:
+        print(f"\nTEST 1 [{curr_time()}] ░░░░░░░░░░░░░░░░░░░░░░░░░░░")
+
+        # Update ticker file.
+        download_tickers()
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+#________________________________________________________________________________________________________________________________________________________________
+
 if __name__ == "__main__":
-    main()
+
+    try:
+        push_terminal_space(terminal_vertical_margin_size)
+
+        main_version_list = {
+            0: main,
+            1: test_main,
+        }
+
+        version = 0
+
+        if version in main_version_list:
+            print(f"\nSTAGE 0 [{curr_time()}] ░░░░░░░░░░░░░░░░░░░░░░░░░░░")
+            print(f"\nRunning main function version: {version}.")
+            main_version_list[version]()
+
+        push_terminal_space(terminal_vertical_margin_size)
+
+    except Exception as e:
+        print(f"Error: {e}")
+        push_terminal_space(terminal_vertical_margin_size)
